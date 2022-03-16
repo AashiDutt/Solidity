a07ee07f241767e14b290fbca1b28ee590b30a55
"Aashi Dutt" <"aashi.dutt3@gmail.com">
2022-03-16 09:54:31 +0530

    Initial commit

---
+++ GlobalObjects.sol
@@ -0,0 +1,47 @@
+// Code for global objects, constructors, modifiers
+
+pragma solidity ^0.5.0;
+
+contract GlobalObjects{
+
+    address owner;
+    
+
+    bool public auctionIsRunning = true;
+
+    constructor() public {
+        owner = msg.sender;
+    }
+
+//Helps to remove duplicates
+    modifier onlyOwner() {
+        if(msg.sender == owner){
+            _;
+        }
+        
+    }
+
+    modifier auctionRunning() {
+        require(auctionIsRunning);
+        _;
+    }
+
+    function setAuctionRunning(bool _auctionIsRunning) onlyOwner public{
+        auctionIsRunning = _auctionIsRunning;
+    }
+
+    function setTheOwner(address _address) onlyOwner public {
+        owner = _address;
+    }
+
+    function paymeSomething() auctionRunning payable public {
+        
+
+    }
+
+    function withdraw(address payable _to) onlyOwner public{
+    
+        _to.transfer(1000000000000000000);   //1 ether --18 zeros
+
+    }
+}
