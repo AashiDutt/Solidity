eb45abbbd2809d3c29d204aa3953b05375a771cd
AashiDutt <aashi.dutt3@gmail.com>
2022-04-06 22:11:21 +0530

    Initial Commit

---
+++ DemoAddress.sol
@@ -0,0 +1,27 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.6;
+
+contract AddressDemo {
+
+    uint public balanceRecieved;
+
+    function recieveMoney() public payable {
+        balanceRecieved += msg.value;
+    }
+
+    function getBalance() public view returns(uint){
+        return address(this).balance;
+    }
+
+    function withdrawMoney() public {
+
+        address payable to = payable(msg.sender);
+
+        to.transfer(this.getBalance());
+    }
+
+    function withdrawMoneyTo(address payable _to) public {
+        _to.transfer(this.getBalance());
+    }
+
+}
